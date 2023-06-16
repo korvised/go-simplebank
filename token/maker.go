e352aaf42cbdd075e78cc95762d84ab4cdb7d463
long_time <apb@1234>
2023-06-16 09:04:36 +0700

    token maker

---
+++ token/maker.go
@@ -0,0 +1,12 @@
+package token
+
+import "time"
+
+// Maker is an interface for manging tokens
+type Maker interface {
+	// CreateToken creates a new token a specific username and duration
+	CreateToken(username string, duration time.Duration) (string, error)
+
+	// VerifyToken Check if the token is valid or not
+	VerifyToken(token string) (*Payload, error)
+}
